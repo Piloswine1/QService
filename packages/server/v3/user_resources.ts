f40326fd83f62e34e1dbae21b04defd2b946685b
Лысов Глеб <lysovg9@gmail.com>
2022-04-27 18:40:55 +0300

    Added: v3 route skelet

---
+++ packages/server/v3/user_resources.ts
@@ -0,0 +1,59 @@
+import { Drash, Redis } from "../deps.ts";
+import { BaseResource } from "../resources/base_resource.ts";
+
+export class RedisWeatherResource extends BaseResource {
+    public paths = this.prefixPaths("api_v3", ["/current"]);
+    client: Redis.Redis | null = null;
+
+    constructor() {
+        super();
+        Redis
+            .connect({
+                hostname: Deno.env.get("REDIS_HOSTNAME") ?? "0.0.0.0",
+                port: Deno.env.get("REDIS_HOSTNAME") ?? "6380",
+            })
+            .then(newClient => this.client = newClient);
+    }
+
+    public async GET(request: Drash.Request, response: Drash.Response) {
+        if (!this.client)
+            throw new Drash.Errors.HttpError(501, "redis client not set");
+
+        const {city} = await request.json();
+
+        if (!city)
+            throw new Drash.Errors.HttpError(301, "city not stated");
+        
+        const existsUnit = await this.client.hexists(city, 'unit');
+        const existsTemperature = await this.client.hexists(city, 'temperature');
+
+        if (!existsUnit || !existsTemperature)
+            throw new Drash.Errors.HttpError(404, "city not found");
+
+        const unit = await this.client.hget(city, 'unit');
+        const temperature = await this.client.hget(city, 'temperature');
+
+        return response.json({
+            city,
+            unit,
+            temperature
+        });
+    }
+
+    public async PUT(request: Drash.Request, response: Drash.Response) {
+        if (!this.client)
+            throw new Drash.Errors.HttpError(501, "redis client not set");
+        
+        const {city, unit = 'celsius', temperature} = await request.json();
+
+        if (!city)
+            throw new Drash.Errors.HttpError(301, "city not stated");
+
+        if (!temperature)
+            throw new Drash.Errors.HttpError(301, "temperature not stated");
+
+        await this.client.hset(city, {unit, temperature});
+
+        return response.json({message: 'weather set'});
+    }
+}
